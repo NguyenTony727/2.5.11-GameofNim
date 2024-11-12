ad9f565fb761a36c43b9e1f7e9757e9acffd94df
NguyenTony727 <nguyen2415@mydusd.org>
2024-11-12 12:42:00 -0800

    Initial Commit

---
+++ GameRunner.java
@@ -0,0 +1,11 @@
+/**
+ * Project 2.5.11
+ *
+ * GameRunner Class for the Game of Nim 
+*/
+public class GameRunner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Game of Nim!");
+        Game game = new Game();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,14 @@
+/*
+ * Activity 2.5.11
+ *
+ * A Player class the Game of Nim
+ */
+public class Player {
+    /* attributes */
+    String name;
+
+    /* constructor */
+    public Player(){
+        
+    }
+}
